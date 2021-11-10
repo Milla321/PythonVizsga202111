5f0b27012202d6f12d8cb134b7677b07d834ac51
Milla321 <71266144+Milla321@users.noreply.github.com>
2021-11-10 22:19:04 +0100

    Add files via upload

--- tesz.py
+++ tesz.py
@@ -1,6 +1,21 @@
 import sikidom
 
 circle=sikidom.kor();
-print("Kérem a"+str(db)+". számot: ", end="")
+print("Kerulet: "+str(circle.kerulet()) )
+print("Terulet: "+str(circle.terulet()) )
+print("Terulet2: "+str(circle.terulet2()) )
 
+circle=sikidom.kor(10);
+print("Kerulet: "+str(circle.kerulet()) )
+print("Terulet: "+str(circle.terulet()) )
+print("Terulet2: "+str(circle.terulet2()) )
 
+haromSzog=sikidom.hszog(5,5,6)
+print()
+print("Háromszog kerulet:"+str(haromSzog.kerulet()))
+print("Háromszog terulet:"+str(haromSzog.terulet()))
+
+haromSzog=sikidom.hszog()
+print()
+print("Háromszog kerulet:"+str(haromSzog.kerulet()))
+print("Háromszog terulet:"+str(haromSzog.terulet()))
